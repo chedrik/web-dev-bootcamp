9152a2cedcdb9494b6cd7e5253e5aa814e66dc1f
Chasen Sherman <csherman16@berkeley.edu>
2021-05-20 07:19:38 -0700

    add update functionality to yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 // MongoDB setup
@@ -22,6 +23,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
 
 // routes
@@ -39,15 +41,27 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', async (req, res) => {
-    const ground = new Campground(req.body.campground);
-    await ground.save();
-    res.redirect(`/campgrounds/${ground._id}`)
+    const grounds = new Campground(req.body.campground);
+    await grounds.save();
+    res.redirect(`/campgrounds/${grounds._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const grounds = await Campground.findById(id);
     res.render('campgrounds/show', { grounds })
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const grounds = await Campground.findById(id);
+    res.render('campgrounds/edit', { grounds })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const grounds = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { runValidators: true, new: true });
+    res.redirect(`/campgrounds/${grounds._id}`)
 })
 
 app.listen(3000, () => {

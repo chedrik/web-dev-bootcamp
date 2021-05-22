870b8208b7c9f23741fb1e9f0e3ff703280477b6
Chasen Sherman <csherman16@berkeley.edu>
2021-05-22 11:16:56 -0700

    move views to ejs-mate based boilerplate

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,5 +1,6 @@
 // imports
 const express = require('express');
+const ejsMate = require('ejs-mate');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 
 // routes

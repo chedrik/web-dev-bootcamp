24fa340402b5ebf6fde6e9c6a10505bb23197271
Chasen Sherman <csherman16@berkeley.edu>
2021-07-05 18:07:46 -0700

    add login redirect to yelpcamp

--- YelpCamp/utils/auth.js
+++ YelpCamp/utils/auth.js
@@ -1,6 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {  // fcn is included in all req by passport!
         req.flash('error', 'You must be signed in.');
+        req.session.returnTo = req.originalUrl;  // for redirect!
         return res.redirect('/login');
     }
     next();

52d0cf3d0146e92048c71b246571397ddd01aea5
Chasen Sherman <csherman16@berkeley.edu>
2021-07-10 10:49:29 -0700

    add map popup

--- YelpCamp/public/js/showMap.js
+++ YelpCamp/public/js/showMap.js
@@ -2,11 +2,15 @@ mapboxgl.accessToken = mapToken;
 const grounds = JSON.parse(campground);
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: grounds.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
 
 const marker = new mapboxgl.Marker()
     .setLngLat(grounds.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h3> ${grounds.title} </h3>`)
+    )
     .addTo(map);

95d5cff6efcb28a1bb35654e8d0be4f0f1895513
Chasen Sherman <csherman16@berkeley.edu>
2021-07-10 09:46:45 -0700

    add img thumbnail transformation

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema; // shorthand
 
+// virtual property to enable cloudinry transform
+const ImageSchema = new Schema({
+    url: String,
+    filename: String,
+})
+
+// remember, virtual isnt actually stored in mongo! just a fcn
+ImageSchema.virtual('thumbnail').get(function () {
+    // "this" refers to the specific image
+    return this.url.replace('/upload', '/upload/w_200'); // 200px transform
+})
+
 const CampgroundSchema = new Schema({
     title: {
         type: String,
@@ -15,12 +27,7 @@ const CampgroundSchema = new Schema({
     location: {
         type: String,
     },
-    images: [
-        {
-            url: String,
-            filename: String,
-        }
-    ],
+    images: [ImageSchema],
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User',
